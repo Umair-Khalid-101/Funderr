7e78b79a4f78d46b7e309729a18da07f28298bf0
Umair-Khalid-101 <umairkhalid.github@gmail.com>
2023-01-31 04:04:59 +0500

    Jan31

--- server/routes/posts-routes.js
+++ server/routes/posts-routes.js
@@ -15,6 +15,7 @@ const {
   postByCategory,
   featuredPostsById,
   deleteFromFeatured,
+  mobileCreateCampaign,
 } = require("../controllers/postsController");
 const authmiddleware = require("../middleware/auth");
 const router = express.Router();
@@ -34,5 +35,6 @@ router.post("/addfavorite", addfavPost);
 router.delete("/deletefromfav/:id", deleteFavPost);
 router.get("/userfavposts", authmiddleware, getUserFavPosts);
 router.delete("/deletefeatured/:id", deleteFromFeatured);
+router.post("/createcampaign", mobileCreateCampaign);
 
 module.exports = router;
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -5,11 +5,13 @@ const {
   currentUser,
   facebookSignup,
   updateProfile,
+  mobileRegister,
 } = require("../controllers/userController");
 const authmiddleware = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/mobileregister", mobileRegister);
 router.post("/facebook/signup", facebookSignup);
 router.post("/auth", auth);
 router.get("/currentuser", authmiddleware, currentUser);
